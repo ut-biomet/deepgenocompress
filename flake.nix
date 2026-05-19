4461235871447ec4e99cb7050c4088d535db2779
Julien Diot <juliendiot@ut-biomet.org>
2026-05-20 08:42:05 +0900

    fix nix cache public key

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     extra-substituters = [
       "https://cache.nixos-cuda.org"
     ];
-    trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+    extra-trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
   };
 
   outputs =
