e6abb7056caaa6f274664c1836e07bd39207d6b2
Julien Diot <juliendiot@ut-biomet.org>
2026-06-01 10:24:17 +0900

    docs: add a readme for explaining how documentation is built

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
 
 # -- Extentions configuration ------------------------------------------------
 intersphinx_mapping = {
