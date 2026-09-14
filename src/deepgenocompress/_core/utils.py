ba71d67556b5b705a514cc6839e7e0725b12db71
Julien Diot <juliendiot@ut-biomet.org>
2026-09-14 12:53:37 +0900

    feat(utils): add examples files to in the package

--- src/deepgenocompress/_core/utils.py
+++ src/deepgenocompress/_core/utils.py
@@ -1,5 +1,8 @@
 from collections.abc import Mapping
 from enum import Enum
+from importlib import resources
+from pathlib import Path
+from pprint import pformat
 from typing import Any
 
 
@@ -37,3 +40,58 @@ def encoding_size(encoding_map: Mapping[Any, list[int] | list[float]]):
         return len(next(iter(encoding_map.values())))
     except StopIteration as e:
         raise ValueError("encoding_map must not be empty") from e
+
+
+class ExampleFiles:
+    """Namespace of example data file paths.
+
+    Examples
+    --------
+    .. jupyter-kernel::
+       :id: ExampleFiles-example
+
+    .. jupyter-execute::
+
+        import pandas as pd
+
+        from deepgenocompress import read_vcf
+        from deepgenocompress.utils import ExampleFiles
+
+        with pd.option_context('display.max_rows', 10, 'display.max_columns', 5):
+            example_csv = pd.read_csv(ExampleFiles().csv)
+            print(example_csv)
+
+            example_vcf = read_vcf(ExampleFiles().vcf)
+            print(example_vcf)
+    """
+
+    _DATA_DIR = resources.files("deepgenocompress._core.data")
+    """Base directory for example data files."""
+
+    @property
+    def csv(self) -> Path:
+        """CSV example file path.
+
+        SNP genotype table. Each row is a named accession/variety (e.g. ``PURPLE``,
+        ``Jefferson``, ``Basmati`` ...), and columns ``V1`` to``V14`` are 14 biallelic
+        SNP markers with values being single-nucleotide genotype calls
+        (``A``, ``C``, ``G``, ``T``), with ``N`` denoting missing values.
+        """
+        return Path(str(self._DATA_DIR / "example_1.csv"))
+
+    @property
+    def vcf(self) -> Path:
+        """VCF example file path.
+
+        Small and simple example VCF (v4.3) file containing 14 biallelic and
+        multiallelic SNP variants across 3 synthetic chromosomes and 50 individuals.
+        """
+        return Path(str(self._DATA_DIR / "example_2.vcf"))
+
+    def __repr__(self):
+        files = {
+            name: getattr(self, name)
+            for name in dir(self)
+            if not name.startswith("_") and not callable(getattr(self, name))
+        }
+        return pformat(files)
--- src/deepgenocompress/utils.py
+++ src/deepgenocompress/utils.py
@@ -3,7 +3,7 @@
 from typing import get_args
 
 from ._core.compression import LayerSizeOption, possible_first_layer_sizes
-from ._core.utils import encoding_size
+from ._core.utils import ExampleFiles, encoding_size
 from ._core.vcf import _MARKER_ID_FORMAT, build_marker_ids
 
 MARKER_ID_FORMATS: tuple[_MARKER_ID_FORMAT] = get_args(_MARKER_ID_FORMAT)
@@ -11,6 +11,7 @@ MARKER_ID_FORMATS: tuple[_MARKER_ID_FORMAT] = get_args(_MARKER_ID_FORMAT)
 
 __all__ = [
     "MARKER_ID_FORMATS",
+    "ExampleFiles",
     "LayerSizeOption",
     "build_marker_ids",
     "encoding_size",
